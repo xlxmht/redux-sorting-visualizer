06b0267420a5afab6fac985139d44ac6c04bc7ff
Maxgrid <xlxmht@gmail.com>
2020-02-06 13:06:38 +0530

    dev tools disabled for prod mode

--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers';
 
+const devTool = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
 export default createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    process.env.NODE_ENV === 'development' ? devTool : (function () { })()
 );
